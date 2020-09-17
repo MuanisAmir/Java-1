c8d43ef9c17ce5207466c7e0ef12b5017f23244d
MuanisAmir <71417244+MuanisAmir@users.noreply.github.com>
2020-09-16 21:44:36 -0400

    Add files via upload

---
+++ EnvironmentWoman.java
@@ -0,0 +1,13 @@
+//Muanis Amir. CMSY-166-002. Environment Woman!
+// New super here is here and let introduce her.
+//Use 3 println line.
+public class EnvironmentWoman {
+        public static void main (String[] args) {
+
+
+
+  System.out.println("Environment Woman is new on the scene!");
+  System.out.println("Enviroment Woman's vision: Her goal is to defeat super villains and to save the environment");
+  System.out.println("Muanis Amir will be assisting Environment Woman as her lead software developer ");
+}
+}
